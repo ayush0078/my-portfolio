f3cb13f5290bdfd09bec79095cd6faa2c496ccac
ayush0078 <ayush0078@gmail.com>
2021-06-23 13:57:21 +0200

    About Me Updated with the CV

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import author from "../IMG-6117.jpg";
+import cv from "../Aayush_Jain_CV_1.1.pdf";
 
 const AboutMe = () => {
     return (
@@ -16,7 +17,7 @@ const AboutMe = () => {
                     <p>I'm a Software Engineer, speacialized in Full Stack Web and Mobile Development. Technologies i use is MERN(MongoDB, Express, ReactJS and NodeJS).</p>
                     <p>Currently, I am pursuing my Master in Applied Computer Science from SRH Hochschule Heidelberg University. I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set.</p>
                     <div className="download-resume">
-                        <a href="#" className="btn-download-cv">Download Resume</a>
+                        <a href={cv} className="btn-download-cv">Download Resume</a>
                     </div>
                 </div>
             </div>
